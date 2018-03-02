d20cc6eedb5aba15e67399df45da67684edd62d2
EC2 Default User <ec2-user@ip-172-31-40-63.us-east-2.compute.internal>
2018-03-02 00:40:45 +0000

    Added docker-compose to the setup

--- setup.sh
+++ setup.sh
@@ -19,3 +19,6 @@ echo "Starting Docker service"
 service docker start
 
 echo "Setup complete"
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
